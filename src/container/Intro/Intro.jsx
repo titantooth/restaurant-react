e76553e164025b2e5da77e0fc12aba3ab2822cc6
titantooth <ali.rabieeie@gmail.com>
2023-08-04 22:10:54 +0330

    implemented overlay hide while playing

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
 
-import './Intro.css';
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const [style, setStyle] = React.useState("app-video-overlay");
+  const vidRef = React.useRef();
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+    if (playVideo) {
+      vidRef.current.pause();
+      setStyle("app-video-overlay");
+
+      // showvideo
+    } else {
+      vidRef.current.play();
+      setStyle("app-video-overlay2");
+
+      // hidevideo
+    }
+  };
+
+  return (
+    <div className="app-video">
+      <video
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+        ref={vidRef}
+      />
+      <div className={style}>
+        <div
+          className="app-video-overlay-circle flex-center"
+          onClick={handleVideo}
+          id="overlay"
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
