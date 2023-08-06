007c090367d2e36aa95bdf51c8ae8d1a789e3549
titantooth <ali.rabieeie@gmail.com>
2023-08-06 22:36:27 +0330

    some debugging with footeroverlay css

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -4,8 +4,8 @@ import "./FooterOverlay.css";
 
 const FooterOverlay = () => (
   <div className="app-footerOverlay">
-    <div className="app-footerOverlay-black"></div>
-    <div className="app-footerOverlay-img app-bg"></div>
+    <div className="app-footerOverlay-black" />
+    <div className="app-footerOverlay-img app-bg" />
   </div>
 );
 
